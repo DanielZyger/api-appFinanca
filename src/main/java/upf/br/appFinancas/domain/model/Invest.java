2bcd4e40025aaf5278a31317dba9fea90e3cf777
DanielZyger <daniel_zyger@hotmail.com>
2023-06-09 07:16:59 -0300

    fix: melhorias pro front e correção de bug

--- src/main/java/upf/br/appFinancas/domain/model/Invest.java
+++ src/main/java/upf/br/appFinancas/domain/model/Invest.java
@@ -7,18 +7,6 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "tb_invest")
 public class Invest extends Account {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     @Column(name = "goal")
     private Float goal;
     
