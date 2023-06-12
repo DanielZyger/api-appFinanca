f98c3bc9e7053060dc2ea784e1abc67f6a9496f6
DanielZyger <daniel_zyger@hotmail.com>
2023-06-12 19:40:23 -0300

    seila

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,6 @@
 INSERT INTO tb_user (name, email, password) VALUES ('daniel', 'daniel@hotmail.com', 'teste');
-INSERT INTO tb_user (name, email, password) VALUES ('teste', 'teste@gmail.com', 'teste123');
 
-
-INSERT INTO tb_category (color, name) VALUES ('Amarelo', 'Alimentacao');
+INSERT INTO tb_category (name, color) VALUES ('Alimentacao', 'Amarelo');
 INSERT INTO tb_category (name, color) VALUES ('Contas', 'Azul');
 INSERT INTO tb_category (name, color) VALUES ('Cartao de Credito', 'Vermelho');
 INSERT INTO tb_category (name, color) VALUES ('Saúde', 'Verde');
