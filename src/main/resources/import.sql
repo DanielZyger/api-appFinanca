7a308a73425ed9bd9c738229d602c918bf3fb357
DanielZyger <daniel_zyger@hotmail.com>
2023-06-18 23:19:58 -0300

    update imports

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,9 +1,9 @@
 INSERT INTO tb_user (name, email, password) VALUES ('daniel', 'daniel@hotmail.com', 'teste');
 
-INSERT INTO tb_category (name, color) VALUES ('Alimentacao', 'Amarelo');
-INSERT INTO tb_category (name, color) VALUES ('Contas', 'Azul');
-INSERT INTO tb_category (name, color) VALUES ('Cartao de Credito', 'Vermelho');
-INSERT INTO tb_category (name, color) VALUES ('Saúde', 'Verde');
-INSERT INTO tb_category (name, color) VALUES ('Lazer', 'Roxo');
-INSERT INTO tb_category (name, color) VALUES ('Transporte', 'Cinza');
+INSERT INTO tb_category (name, color) VALUES ('Alimentacao', 'yellow');
+INSERT INTO tb_category (name, color) VALUES ('Contas', 'blue');
+INSERT INTO tb_category (name, color) VALUES ('Cartao de Credito', 'red');
+INSERT INTO tb_category (name, color) VALUES ('Saúde', 'green');
+INSERT INTO tb_category (name, color) VALUES ('Lazer', 'purple');
+INSERT INTO tb_category (name, color) VALUES ('Transporte', 'grey');
 
